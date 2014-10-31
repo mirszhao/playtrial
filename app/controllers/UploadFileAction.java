6c6f6950c76f54a6d77f2bb3c4f63326c74c351e
mirs <simple_mirs@163.com>
2014-10-31 12:05:09 +0800

    file upload

---
+++ app/controllers/UploadFileAction.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * 上传文件
+ * @author guangpeng.zhao
+ *
+ */
+public class UploadFileAction extends Controller{
+	
+	/**
+	 * 加载上传信息页面
+	 * @return
+	 */
+	public static Result loadUploadPage(){
+		return TODO;
+	}
+	
+	/**
+	 * 上传文件请求
+	 * @return
+	 */
+	public static Result uploadFile(){
+		return TODO;
+		
+	}
+}
